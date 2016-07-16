eb74d398037b81a42ab647d1553e3d57d7f2c0e6
Tomas <tomasnavickas0@gmail.com>
2016-07-16 15:30:36 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Atmaz-Website
+# This is a super fancy front-end for the Atmaz project. Flying stuff everywhere.
---
+++ app/home/home.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var app = angular.module('myApp.home', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/home', {
+    templateUrl: 'home/home.html',
+    controller: 'HomeCtrl'
+  });
+}]);
+
+app.controller('HomeCtrl', ['$scope', function($scope) {
+  $scope.text = "Atmaz mother fuckers.";
+
+  $('.overlay').css('display', 'none');
+
+  $scope.alert = function() {
+    if($('.overlay').css('display') == 'none') {
+      $('.overlay').fadeIn('slow').delay(2000).fadeOut('slow');
+    }
+  }
+}]);
